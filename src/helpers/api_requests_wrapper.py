c2de78aaf0c12231e8632080a98b1463cfbed3ab
rajanichoudhary <rajaniusaindia@gmail.com>
2024-08-05 19:39:38 -0400

    Added helpers

--- src/helpers/api_requests_wrapper.py
+++ src/helpers/api_requests_wrapper.py
@@ -5,5 +5,34 @@ import requests
 
 # Create request function to use again and again
 # Code reusability
-def get_request():
-    pass
+# a custom function I created - takes URL, Auth
+# we added a wrapper - we use requests() again and again
+def get_request(url, auth):
+    response = requests.get(url=url, auth=auth)
+    return response.json()
+
+def post_request(url, auth, headers, payload,in_json):
+    post_response = requests.post(url=url, headers=headers,auth=auth, data=json.dumps(payload))
+    if in_json is True:
+        return post_response.json()
+    return
+
+def patch_requests(url, headers, auth, payload, in_json):
+    patch_response_data = requests.patch(url=url, headers=headers, auth=auth, data=json.dumps(payload))
+    if in_json is True:
+        return patch_response_data.json()
+    return
+
+def put_requests(url, headers, auth, payload, in_json):
+    put_response_data = requests.put(url=url, headers=headers, auth=auth, data=json.dumps(payload))
+    if in_json is True:
+        return put_response_data.json()
+    return put_response_data
+
+def delete_requests(url, headers, auth, in_json):
+    delete_response_data = requests.delete(url=url, headers=headers, auth=auth)
+    if in_json is True:
+        return delete_response_data.json()
+    return delete_response_data
+
+
--- tests/crud/test_create_booking.py
+++ tests/crud/test_create_booking.py
@@ -1,9 +1,15 @@
-# CHeck out previous test case Lab187 - conftest.py - create_booking_id
+# Check out previous test case Lab187 - conftest.py - create_booking_id
 # We divided it into 5 parts -
 # Create Helpers - requests wrapper, verification and the payload manager
 
 import allure
 import pytest
+
+from src.constants.api_constants import APIConstants
+from src.helpers.api_requests_wrapper import post_request
+from src.helpers.common_verification import verfiy_http_status_code, verify_json_key_for_not_null
+from src.helpers.payload_manager import payload_create_booking
+from src.utils.utils import Utils
 
 class TestCreateBooking(object):
     @pytest.mark.positive
@@ -12,5 +18,15 @@ class TestCreateBooking(object):
         "Creating a Booking from the payload and verify that booking id should not be null")
 
     def test_create_booking_positive(self):
-        pass
+        response = post_request(url=APIConstants().url_create_booking(),
+                                auth=None,
+                                headers=Utils().common_headers_json(),
+                                payload=payload_create_booking(),
+                                in_json=False)
+
+        verfiy_http_status_code(response_data=response, expect_data=200)
+        verify_json_key_for_not_null(response.json()["bookingid"])
+
+
+    # 1st it was - pass - future implementation
     # run this test case first before proceeding with any push of code to the github
